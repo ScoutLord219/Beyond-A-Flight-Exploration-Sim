bea299a28651436d9b39029fe3fcc1c62b45127a
s_o0om <59508379+ScoutLord219@users.noreply.github.com>
2024-08-25 15:05:57 -0500

    Scripts used for version 1 of Beyond

---
+++ _ui.gd
@@ -0,0 +1,8 @@
+extends Control
+
+@onready var camera_2d = $"../Player/Camera2D"
+@onready var control = $"."
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#control.position = camera_2d.position
---
+++ music.gd
@@ -0,0 +1,6 @@
+extends AudioStreamPlayer2D
+
+
+const INITIAL = preload("res://assets/audio/initial.mp3")
+const BEYOND = preload("res://assets/audio/beyond.mp3")
+
